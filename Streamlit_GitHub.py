cc952926e82a762f3e1ba6698c1e18f886790b30
peipei920122 <169127260+peipei920122@users.noreply.github.com>
2024-06-21 22:22:14 +0800

    Add files via upload

--- Streamlit_GitHub.py
+++ Streamlit_GitHub.py
@@ -15,9 +15,9 @@ import streamlit.components.v1 as stc
 
 ###### (1) 開始設定 ######
 html_temp = """
-    <div style="background-color:#00bfff;padding:10px;border-radius:10px">
-    <h1 style="color:#ff6347;text-align:center;">聯電股票金融資料視覺化呈現 (金融看板) </h1>
-    <h2 style="color:#ff6347;text-align:center;">UMC Financial App </h2>
+    <div style="background-color:#deb887;padding:10px;border-radius:10px">
+    <h1 style="color:#000000;text-align:center;">聯電股票金融資料視覺化呈現 (金融看板) </h1>
+    <h2 style="color:#000000;text-align:center;">UMC Financial App </h2>
     </div>
 """
 
