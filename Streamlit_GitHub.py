67cd4eee0157464b7f5acdd4d4eed9ad4e959f2e
peipei920122 <169127260+peipei920122@users.noreply.github.com>
2024-06-21 23:06:07 +0800

    Add files via upload

--- Streamlit_GitHub.py
+++ Streamlit_GitHub.py
@@ -302,7 +302,7 @@ with st.expander("K線圖, 長短 RSI"):
     
     fig2.add_trace(go.Scatter(x=KBar_df['Time'][last_nan_index_RSI+1:], y=KBar_df['RSI_long'][last_nan_index_RSI+1:], mode='lines',line=dict(color='#8fbc8f', width=2), name=f'{LongRSIPeriod}-根 K棒 移動 RSI'), 
                   secondary_y=False)
-    fig2.add_trace(go.Scatter(x=KBar_df['Time'][last_nan_index_RSI+1:], y=KBar_df['RSI_short'][last_nan_index_RSI+1:], mode='lines',line=dict(color='#ffd700', width=2), name=f'{ShortRSIPeriod}-根 K棒 移動 RSI'), 
+    fig2.add_trace(go.Scatter(x=KBar_df['Time'][last_nan_index_RSI+1:], y=KBar_df['RSI_short'][last_nan_index_RSI+1:], mode='lines',line=dict(color='#f5deb3', width=2), name=f'{ShortRSIPeriod}-根 K棒 移動 RSI'), 
                   secondary_y=False)
     
     fig2.layout.yaxis2.showgrid=True
